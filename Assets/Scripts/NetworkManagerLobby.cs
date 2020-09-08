cdb4d011eec157f8f8952d607c8af976d8e42f41
David <Torres>
2020-09-08 12:01:01 -0500

    first commit

---
+++ Assets/Scripts/NetworkManagerLobby.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using System;
+using UnityEngine.SceneManagement;
+using System.Linq;
+public class NetworkManagerLobby : NetworkManager
+{
+    [Header("Added features")]
+    [SerializeField] private int minPlayers = 2;
+    [Scene] [SerializeField ] private string menuScene = string.Empty;
+
+    [Header("Room")]
+    [SerializeField] private NetworkRoomPlayerLobby roomPlayerPrefab = null;
+
+    public static event Action OnClientConnected;
+    public static event Action OnClientDisconnected;
+
+
+    public List<NetworkRoomPlayerLobby> RoomPlayers { get; } = new List<NetworkRoomPlayerLobby> ();
+
+    #region Load From "resources" folder all game objects
+    public override void OnStartServer ()
+    {
+        spawnPrefabs = Resources.LoadAll<GameObject> ( "SpawnablePrefabs" ).ToList ();
+    }
+
+    public override void OnStartClient ()
+    {
+        var spawnablePrefabs = Resources.LoadAll<GameObject> ( "SpawnablePrefabs" );
+
+        foreach ( var prefab in spawnablePrefabs )
+        {
+            ClientScene.RegisterPrefab ( prefab );
+        }
+    }
+
+    #endregion
+
+    public override void OnClientConnect ( NetworkConnection conn )
+    {
+        base.OnClientConnect ( conn );
+        OnClientConnected?.Invoke ();
+    }
+
+    public override void OnClientDisconnect ( NetworkConnection conn )
+    {
+        base.OnClientDisconnect ( conn );
+
+        OnClientDisconnected?.Invoke ();
+    }
+
+    public override void OnServerConnect ( NetworkConnection conn )
+    {
+        if ( numPlayers >= maxConnections ) // Too  many players make disconnection
+        {
+            conn.Disconnect ();
+            return;
+        }
+
+        if ( "Assets/Scenes/" + SceneManager.GetActiveScene ().name + ".unity" != menuScene )
+        {
+            conn.Disconnect ();
+            return;
+        }
+    }
+
+    public override void OnServerAddPlayer ( NetworkConnection conn )
+    {
+        if ( "Assets/Scenes/" + SceneManager.GetActiveScene ().name + ".unity" == menuScene )
+        {
+            bool isLeader = RoomPlayers.Count == 0;
+
+            NetworkRoomPlayerLobby roomPlayerInstance = Instantiate ( roomPlayerPrefab );
+
+            roomPlayerInstance.IsLeader = isLeader;
+
+            NetworkServer.AddPlayerForConnection ( conn , roomPlayerInstance.gameObject );
+        }
+    }
+
+    public override void OnServerDisconnect ( NetworkConnection conn )
+    {
+
+        if ( conn.identity != null )
+        {
+            var player = conn.identity.GetComponent<NetworkRoomPlayerLobby> ();
+            RoomPlayers.Remove ( player );
+
+            NotifyPlayersOfReadyState ();
+        }
+
+        base.OnServerDisconnect ( conn );
+    }
+
+    public override void OnStopServer ()
+    {
+        RoomPlayers.Clear ();
+    }
+
+    public void NotifyPlayersOfReadyState ()
+    {
+        foreach ( var player in RoomPlayers )
+        {
+            player.HandleReadyToStart ( IsReadyToStart () );
+        }
+    }
+
+    private bool IsReadyToStart ()
+    {
+        if ( numPlayers > minPlayers )
+        {
+            foreach ( var player in RoomPlayers )
+            {
+                if ( !player.IsReady )
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+}
---
+++ Assets/Scripts/NetworkRoomPlayerLobby.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.SceneManagement;
+using System;
+using System.Linq;
+using TMPro;
+using UnityEngine.UI;
+public class NetworkRoomPlayerLobby : NetworkBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private GameObject lobbyUI = null;
+    [SerializeField] private TMP_Text[] playerNameTexts = new TMP_Text[4];
+    [SerializeField] private TMP_Text[] playerReadyTexts = new TMP_Text[4];
+    [SerializeField] private Button startGameButton = null;
+
+    [SyncVar(hook = nameof(HandleDisplayNameChanged))]
+    public string DisplayName ="Loading...";
+    [SyncVar ( hook = nameof ( HandleDisplayNameChanged ) )]
+
+    public bool IsReady = false;
+
+    private bool isLeader;
+
+    public bool IsLeader
+    {
+        set
+        {
+            isLeader = value;
+            startGameButton.gameObject.SetActive ( value );
+        }
+    }
+
+    private NetworkManagerLobby room;
+
+    private NetworkManagerLobby Room
+    {
+        get
+        {
+            if ( room == null )
+            {
+                return room = NetworkManager.singleton as NetworkManagerLobby;
+            }
+            return room;
+        }
+    }
+}
